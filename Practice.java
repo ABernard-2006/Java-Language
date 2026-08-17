6bafce0a0a367c7631066340ea8e3d711589251b
Bernard Aurellano <233678819+ABernard-2006@users.noreply.github.com>
2026-08-17 22:36:10 +0800

    Java Practice

--- Practice.java
+++ Practice.java
@@ -12,6 +12,21 @@ public class Practice{
 		System.out.println(fullName);
 		System.out.println();
 
+		double a = 10.54321;
+		System.out.println(a);
+
+		float b = 10.54321f;
+		System.out.println(b);
+
+		char c = 65;
+		System.out.println(c);
+
+		double myDouble = 65.789;
+		char typeCast = (char) myDouble;
+		System.out.println(typeCast);
+
+		String AB = "Hello";
+		
 		int x, y, z;
 		x = y = z = 10;
 		System.out.println(x+y+z);
---
+++ PracticeStudentRecordCard.java
@@ -0,0 +1,14 @@
+
+
+public class PracticeStudentRecordCard {
+    public static void main (String[] args)
+    {
+        var studentID = 2025;
+        var score = 48.5;
+        var grade = 'B';
+
+        System.out.println(studentID);
+        System.out.println(score);
+        System.out.println(grade);
+    }
+}
